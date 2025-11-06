ead1e0fcc94649d1b4903066eb45a02d21943d05
JS66KIM <junseong6980@gmail.com>
2025-11-06 15:00:46 +0900

    new foofoo and foofoofoo are dafined

--- src/func1.c
+++ src/func1.c
@@ -3,3 +3,11 @@
 void foo(void){
     printf("Hello, I'm foo my bro! \n");
 }
+
+void fooofoo(void){
+    printf("Hello, I'm fooofoo! \n");
+}
+
+void fooofoofoo(void){
+    printf("Hello, I'm fooofoofoo! \n");
+}
